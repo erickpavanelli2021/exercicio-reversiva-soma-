632dd3071bc9f1243381cb0db3e8a842e04cec2b
erick.pavanelli <erick.pavanelli@Analista-02.mendel.com>
2021-08-20 08:47:26 -0300

    Soma com input

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,4 +1,5 @@
 package view;
+import javax.swing.JOptionPane;
 
 //Importando a classe de controle do Fatorial
 import controller.SomaController;
@@ -9,7 +10,8 @@ public class Principal {
 		//Método construtor
 		SomaController ft = new SomaController();
 		//Definindo um valor para o fatorial
-		int soma = 10;
+		int soma = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o número desejado: "));
+		//int num = Integer.parseInt(numero);
 		//variavel que recebe o resultado
 		int resultado = ft.result(soma);
 		System.out.println("Resultado: "+resultado);
