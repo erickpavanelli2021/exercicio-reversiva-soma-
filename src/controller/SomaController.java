2a0861d1830e72d6f91a735380ee1550896e2aea
erick.pavanelli <erick.pavanelli@Analista-02.mendel.com>
2021-08-20 09:03:38 -0300

    teste2

--- src/controller/SomaController.java
+++ src/controller/SomaController.java
@@ -7,10 +7,10 @@ public class SomaController {
 		}
 		
 		public int result(int soma) {
-			//Condição de parada = se o fatorial for 0, deverá retornar 0
+			//Condição de parada = se o result for 0, deverá retornar 0
 		 if (soma == 0) {
 	      return 0;	
-	      //Caso seja diferente de 0, deverá somar fatorial + (fatorial - 1)
+	      //Caso seja diferente de 0, deverá somar soma + (soma - 1)
 		 }else {
 			 //Fatorial + (Fatorial - 1)
 			 return soma + result(soma - 1);
